67edb63584cc8d4d0fa5a159cb78c7b9035eed06
prussian <generalunrest@airmail.cc>
2016-05-11 22:41:48 -0400

    changed config back to descriptive old self

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 # SETTINGS
 server="irc.rizon.net"
-nickname="the8ball1"
+nickname="the8ball"
 # space sep list of chans
-channels=('#/cum/')
+channels=('#chan1' '#chan2' '#etc')
 # 8ball responses
 t8ball="8ball-resp.txt"
 infile="/tmp/in-8ballbot"
